2fcfee075219108899ef57c8ee9076ec367171c9
Yu-Jen Chang <juzen2003@gmail.com>
2017-06-27 00:07:44 -0700

    0.0.0.1 - initial draft * 2 imagePicker buttons ready, one from camera, one from photo lib * image would display on image once it's selected by user

--- MemeMeV1/ViewController.swift
+++ MemeMeV1/ViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var ImagePickView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        // diabled camera button if no camera available
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
 
+    @IBAction func pickAnImageFromAlbum(_ sender: Any) {
+        presentImagePicker(.photoLibrary)
+    }
+    
+    @IBAction func pickAnImageFromCamera(_ sender: Any) {
+        presentImagePicker(.camera)
+    }
+    
+    // present image picker for different sourceType
+    func presentImagePicker(_ source: UIImagePickerControllerSourceType) {
+        let imagePickerView = UIImagePickerController()
+        imagePickerView.delegate = self
+        imagePickerView.sourceType = source
+        self.present(imagePickerView, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            self.ImagePickView.image = image
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+ 
 
 }
 
